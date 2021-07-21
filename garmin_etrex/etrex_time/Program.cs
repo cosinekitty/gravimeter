4f9a46e61533971f2e6e5f4efac19c6e90d78664
Don Cross <cosinekitty@gmail.com>
2021-07-21 17:21:19 -0400

    Comparing eTrex time stamps with computer time.
    It's not that great. The delta time values are
    wider than 50 milliseconds. That is a shame.

--- garmin_etrex/etrex_time/Program.cs
+++ garmin_etrex/etrex_time/Program.cs
@@ -13,13 +13,36 @@ namespace etrex_time
                 string line;
                 while (null != (line = port.ReadLine()))
                 {
+                    // Grab current date and time according to the computer.
+                    // On my system, this is kept in sync by ntp.
+                    DateTime now = DateTime.UtcNow;
+
                     line = line.Trim();
-                    Console.WriteLine(line);
-                    // @210721210215N2842048W08107262G007+00013E0000N0000D0001
-                    // @yymmddhhmmssNlat----Wlon-----G007+00013E0000N0000D0001
+
+                    // Parse date and time from the string.
+                    DateTime gpsTime = ParseDateTime(line);
+
+                    // Print the discrepancy.
+                    TimeSpan dt = gpsTime - now;
+                    Console.WriteLine("{0} {1}", line, dt);
                 }
             }
             return 0;
+        }
+
+        static DateTime ParseDateTime(string line)
+        {
+            if (line.Length != 55 || line[0] != '@')
+                throw new ArgumentException("Invalid Garmin eTrex string format");
+
+            int year = 2000 + int.Parse(line.Substring(1, 2));
+            int month = int.Parse(line.Substring(3, 2));
+            int day = int.Parse(line.Substring(5, 2));
+            int hour = int.Parse(line.Substring(7, 2));
+            int minute = int.Parse(line.Substring(9, 2));
+            int second = int.Parse(line.Substring(11, 2));
+            var gpsTime = new DateTime(year, month, day, hour, minute, second, DateTimeKind.Utc);
+            return gpsTime;
         }
     }
 }
