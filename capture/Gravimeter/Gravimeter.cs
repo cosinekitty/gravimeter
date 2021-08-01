6163b2ff560f2b0287f920edb2cf7b9bd9fb5c48
Don Cross <cosinekitty@gmail.com>
2021-08-01 13:42:44 -0400

    Starting to write C# data capture program.

---
+++ capture/Gravimeter/Gravimeter.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CosineKitty.Gravimetry
+{
+    public class Gravimeter : IDisposable
+    {
+        public Gravimeter(string serialPortPath)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ capture/GravimeterCapture/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CosineKitty.Gravimetry
+{
+    class Program
+    {
+        const string UsageText = @"
+USAGE:
+
+GravimeterCapture /dev/{serialport} terminal
+    Opens a raw terminal session where the user can send
+    commands and receive responses from the gravimeter.
+";
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 2 && args[1] == "terminal")
+                return RawTerminalSession(args[0]);
+
+            Console.WriteLine("{0}", UsageText);
+            return 1;
+        }
+
+        static int RawTerminalSession(string serialport)
+        {
+            return 0;
+        }
+    }
+}
